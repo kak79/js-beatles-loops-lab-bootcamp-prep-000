93ef5bca561514e8b138fa18b8f7e7e5b03d8353
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-16 15:08:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function theBeatlesPlay(musicians, instruments) {
 function johnLennonFacts(array){
   let i = 0;
   while ( i < array.length){
-    console.log( i++   +`"!!!"`  );
+    console.log(   +`"!!!"`  );
     
   }
   
