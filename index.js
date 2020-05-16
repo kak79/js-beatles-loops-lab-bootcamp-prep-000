724790594094671b0019ab879fdcde14fc68372e
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-16 14:00:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,26 +4,20 @@ function theBeatlesPlay(musicians, instruments) {
 
   for (let i = 0; i < musicians.length; i++) {
 
-    if (i === 0) {
-      Object.assign({}, "Beatles", { ["John Lennon plays Guitar" });
-      
-    }
-    else if (i === 1) {
-      Object.assign({}, "Beatles", { ["Paul McCartney plays Bass Guitar" });
 
-    }
-    else if (i === 2) {
-      Object.assign({}, "Beatles", { ["George Harrison plays Lead Guitar" });
 
-    }
-    else {
-      (i === 3);
-      Object.assign({}, "Beatles", { ["Ringo Starr plays Drums" });
 
-    }
-  }
+        if (i <   ){
+    
+    
+
+
+
+      } 
+        else {
+     
+   
+   }
   
-  
- // Object.musicians().forEach(function ())
-  //console.log(musicians + " plays " + instruments);
+ 
 }
