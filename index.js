5bca4f3f9aa3cc8e32f6a72cf8595211acd6e043
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-16 13:39:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+
+  for (let i = 0; i < 4; i++) {
+
+    if (i === 0) {
+      Object.assign({}, "Beatles", { ["John Lennon"]: "Guitar" });
+      
+    }
+    else if (i === 1) {
+      Object.assign({}, "Beatles", { ["Paul McCartney"]: "Bass Guitar" });
+
+    }
+    else if (i === 2) {
+      Object.assign({}, "Beatles", { ["George Harrison"]: "Lead Guitar" });
+
+    }
+    else {
+      (i === 3);
+      Object.assign({}, "Beatles", { ["Ringo Starr"]: "Drums" });
+
+    }
+  }
+  
+  
+  Object.musicians().forEach(function ())
+  console.log(musicians + " plays " + instruments);
+}
